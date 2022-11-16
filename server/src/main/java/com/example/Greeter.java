fbea8f34e1b816aeffa74740df4a2e56ce07e2b0
chessi awa <chessiawa77@gmail.com>
2022-11-15 19:11:28 -0600

    testing scan

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,21 +1,85 @@
+pipeline {
+  agent any
+  tools {
+      maven 'localMaven'
+      jdk 'localJDK'
+  }
+  stages {
+      stage('Git checkout') {
+          steps {
+              git branch: 'main', url: 'https://github.com/cvamsikrishna11/Jenkins-CI-CD-Pipeline-Project-V1.git'
+              sh 'java -version'
+          }
+      }
+      stage('Build') {
+          steps {
+              sh "mvn clean package"
+          }
+          post {
+              success {
+                 archiveArtifacts artifacts: '**/*.war', followSymlinks: false
+              }
+          }
+      }
+  stage('Unit Test'){
+      steps {
+          sh 'mvn test'
+      }
+  }
+  stage('Integration Test'){
+      steps {
+        sh 'mvn verify -DskipUnitTests'
+      }
+  }
+  stage ('Checkstyle Code Analysis'){
+      steps {
+          sh 'mvn checkstyle:checkstyle'
+      }
+      post {
+          success {
+              echo 'Generated Analysis Result'
+          }
+      }
+  }
+  stage ('Sonarqube scan'){
+      steps {
+            sh '''
+            mvn sonar:sonar \
+            -Dsonar.projectKey=javawebapp \
+            -Dsonar.host.url=http://xx.xx.xx.xx:9000 \
+            -Dsonar.login=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+          '''
+      }
+  }
+  }
+}
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+
+
+Levi Awa
+7:06 PM
 package com.example;
-
 /**
- * This is a class.
- */
+* This is a class.
+*/
 public class Greeter {
-
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
-
- /**
-   * This is a method.
-   */
-  public final String greet(final String someone) {
-    return String.format("Hello Avinash, %s!", someone);
-  }
+/**
+ * This is a constructor.
+ */
+public Greeter() {
+}
+/**
+ * This is a method.
+ */
+public final String greet(final String someone) {
+  String DB_PASSWORD = "Admin@12345";
+  return String.format("Hello Levi, %s!", someone);
+}
 }
